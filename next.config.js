ffb9d8bf21ad77e5f31cbb69555c57384a2e7e6b
khs <ghgfhfg2@naver.com>
2022-11-22 16:51:27 +0900

    .

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   images: {
     loader: "imgix",
     path: "/",
   },
-};
+});
+
+module.exports = nextConfig;
--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,30 @@ class MyDocument extends Document {
     return (
       <Html lang="ko">
         <Head>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6788425959877259"
-     crossOrigin="anonymous"></script>
+          <title>play vote | 투표를 합시다.</title>
+          <meta
+            name="keywords"
+            content="vote,투표,점심메뉴,선택장애,비밀투표,공개투표"
+          />
+          <meta
+            name="description"
+            content="결정이 필요한 순간 투표를 해보세요!"
+          />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta property="og:site_name" content="play vote | 투표를 합시다." />
+          <meta property="og:title" content="play vote | 투표를 합시다." />
+          <meta property="og:url" content="https://vote.sooyadev.com/" />
+          <meta property="og:type" content="website" />
+          <meta name="theme-color" content="#3182CE" />
+          <link rel="manifest" href="/manifest.json" />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6788425959877259"
+            crossOrigin="anonymous"
+          ></script>
         </Head>
         <body>
           <Main />
